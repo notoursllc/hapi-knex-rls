ecb68263c03597bbe09fbac661611d786934e8de
gbru <gbru@users.noreply.github.com>
2023-01-16 13:31:06 -0800

    refactor: Remove unused code

--- server/db/migrations/1_create_app_user.js
+++ server/db/migrations/1_create_app_user.js
@@ -4,14 +4,11 @@
  */
 exports.up = function(knex) {
     return Promise.all([
-        // knex.raw(`CREATE USER ${process.env.DATA_DB_APPUSER} WITH LOGIN PASSWORD '${process.env.DATA_DB_APPUSER_PASSWORD}'`),
         knex.raw(`GRANT ALL PRIVILEGES ON DATABASE ${process.env.DATA_DB_NAME} to ${process.env.DATA_DB_APPUSER}`),
 
+        // The DB user that will be able to bypass RLS:
         knex.raw(`GRANT ALL PRIVILEGES ON DATABASE ${process.env.DATA_DB_NAME} to ${process.env.DATA_DB_APPUSER_BYPASSRLS}`),
         knex.raw(`ALTER ROLE ${process.env.DATA_DB_APPUSER_BYPASSRLS} BYPASSRLS`),
-
-        // dont do this.
-        // knex.raw(`SET ROLE ${process.env.DATA_DB_APPUSER}`)
     ]);
 };
 
