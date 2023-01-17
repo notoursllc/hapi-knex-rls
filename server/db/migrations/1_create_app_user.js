367148c379714f4d4f457833353f461de3a3480e
gbru <gbru@users.noreply.github.com>
2023-01-16 21:32:12 -0800

    refactor: Users should be added to a group first thing, so privilege granting can done on the group instead of individually

--- server/db/migrations/1_create_app_user.js
+++ server/db/migrations/1_create_app_user.js
@@ -4,15 +4,16 @@
  */
 exports.up = function(knex) {
     return Promise.all([
-        knex.raw(`GRANT ALL PRIVILEGES ON DATABASE ${process.env.DB_NAME} to ${process.env.DB_APPUSER}`),
-
-        // The DB user that will be able to bypass RLS:
-        knex.raw(`GRANT ALL PRIVILEGES ON DATABASE ${process.env.DB_NAME} to ${process.env.DB_APPUSER_BYPASSRLS}`),
-        knex.raw(`ALTER ROLE ${process.env.DB_APPUSER_BYPASSRLS} BYPASSRLS`),
-
+        // put our two 'app' users into a group so it's easier to grand privileges to them in subsequent migrations
         knex.raw(`
             GRANT ${process.env.DB_APP_ALL_USERS_GROUP_NAME} TO ${process.env.DB_APPUSER}, ${process.env.DB_APPUSER_BYPASSRLS}
         `),
+
+        // grant all privs to the group, so both users get them
+        knex.raw(`GRANT ALL PRIVILEGES ON DATABASE ${process.env.DB_NAME} to ${process.env.DB_APP_ALL_USERS_GROUP_NAME}`),
+
+        // The DB user defined by `DB_APPUSER_BYPASSRLS` will be able to bypass RLS:
+        knex.raw(`ALTER ROLE ${process.env.DB_APPUSER_BYPASSRLS} BYPASSRLS`),
     ]);
 };
 
